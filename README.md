af533654edd32b45b2009c22cf9c1ec6079066fc
bwelds <bwelds@gmail.com>
2012-11-20 11:22:16 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+ToListDo
+
+ToListDo is a to-do application developed as part of my quest to learn Rails. Assigned to me by my mentor, the only restriction was to not "cheat" by looking at other people's to-do apps.
+
+So onwards!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tolistdo::Application.config.secret_token = '01bde1b79428734a9e7f3cf916daae8a09adbc6f767ceeb3bc7668b2800e99e63c591da2e3bc9ade85804a34799464faf0772bc7ee805975182457b0480b4395'
