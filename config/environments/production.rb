3e5ff33b22a2498343e6c359566adc80cfe2dccf
bwelds <bwelds@gmail.com>
2012-11-20 22:32:15 -0500

    More heroku mail setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,12 +67,12 @@ Tolistdo::Application.configure do
 
   # Set to actual host
   # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.default_url_options = {:host => 'yourdomain.com'}
+  config.action_mailer.default_url_options = {:host => 'obscure-retreat-4338.herokuapp.com/'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
-  domain: "example.com",
+  domain: "gmail.com",
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: ENV["GMAIL_USERNAME"],
